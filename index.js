b5f56c223d61533150c02a88f88cb10e8d6cf831
dikshabs <57285744+dikshabs@users.noreply.github.com>
2022-01-27 13:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const  cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-const db = crud-mysql.createConnection({
+const db =  mysql.createConnection({
   host     : process.env.localhost,
   user     : process.env.diksha,
   password : process.env.amanni168375,
