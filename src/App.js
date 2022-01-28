ff69b5a1893ea0e8d65c7dd25a0ed59c955eec3f
dikshabs <57285744+dikshabs@users.noreply.github.com>
2022-01-28 10:58:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   
   const addEmployee =() => {
       console.log(name);
-      Axios.post("http://diksha@localhost:3001/create", {
+      Axios.post("http://localhost:8080/create", {
         name:name,
         age:age, 
         address:address,
